b01527e6a65b972415853a60361419a109fa5880
Dipak Pokharel <104352102+dipakPJS@users.noreply.github.com>
2024-03-03 13:23:59 +0545

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,84 @@
- import './Navbar.css';
+import { useState } from 'react';
+import './Navbar.css';
+import { ImCross } from "react-icons/im";
+import { FaBarsStaggered } from "react-icons/fa6";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="flex justify-between items-center py-4 px-10 bg-transparent">
-      <div className="flex items-center">
-        <div className="text-white nav-logo">Liss-olution</div>
+    <nav className="bg-transparent sm:bg-blue950">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Liss-Olution</span>
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          onClick={toggleMenu}
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm   rounded-lg md:hidden"
+          aria-controls="navbar-default"
+          aria-expanded={isOpen ? 'true' : 'false'}
+        >
+          <span className="sr-only">{isOpen ? 'Close main menu' : 'Open main menu'}</span>
+          {isOpen ? (
+         <ImCross className='text-xl text-blue-800'/>
+          ) : (
+          
+            <FaBarsStaggered className='text-[50px] text-blue-800'/>
+          
+          )}
+        </button>
+        <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
+          <ul className="font-large text-xl flex flex-col p-4 md:p-0 mt-6 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+            <li>
+              <a
+                href="#"
+                className="block py-2 font-bold px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                aria-current="page"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul className="flex justify-center items-center space-x-8">
-        <li><a href="#" className="text-white hover:text-gray-300  ">Home</a></li>
-        <li><a href="#" className="text-white hover:text-gray-300 " >Explore</a></li>
-        <li><a href="#" className="text-white hover:text-gray-300   ">About</a></li>
-        <li><a href="#" className="text-white hover:text-gray-300   ">Services</a></li>
-        <li><a href="#" className="text-white hover:text-gray-300   ">Privacy Policy</a></li>
-        <li><a href="#" className="btn text-white rounded-full py-3 px-6 contact font-bold bg-[#0077FF] hover:bg-[#3073c0] ">Contact</a></li>
-      </ul>
     </nav>
   );
 }
